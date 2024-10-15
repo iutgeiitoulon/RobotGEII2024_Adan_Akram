5ebaa845001e89d3599ec171a4b55d0ef972bafd
Table 7 <touloniut@gmail.com>
2024-10-15 17:14:52 +0200

    15/10.1

---
+++ C#/RobotInterface/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Printing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+namespace RobotInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        bool a = false;
+        private void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            buttonSend.Background = Brushes.Beige;
+            SendMessage();
+        }
+
+        private void TextBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+
+        private void SendMessage()
+        {
+            textboxReception.Text += "Reçu :"+textboxEmission.Text + " \n";
+            textboxEmission.Text = "";
+        }
+
+        private void buttonClear_Click(object sender, RoutedEventArgs e)
+        {
+            textboxReception.Clear();
+        }
+
+
+
+    }
+}
