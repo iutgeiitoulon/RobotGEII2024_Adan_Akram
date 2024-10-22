08d94c843b3704032c8ced4fe2bd45728431b3bb
Table 7 <touloniut@gmail.com>
2024-10-22 16:27:31 +0200

    22/10

--- C#/RobotInterface/RobotInterface/MainWindow.xaml.cs
+++ C#/RobotInterface/RobotInterface/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
 using ExtendedSerialPort_NS;
+using System.Windows.Threading;
 namespace RobotInterface
 {
     /// <summary>
@@ -19,14 +20,33 @@ namespace RobotInterface
     public partial class MainWindow : Window
     {
         ExtendedSerialPort serialPort1;
-
+        string receivedText;
+        DispatcherTimer timerAffichage;
 
         public MainWindow()
         {
             InitializeComponent();
-            serialPort1 = new ExtendedSerialPort("COM6", 115200, Parity.None, 8, StopBits.One);
+            serialPort1 = new ExtendedSerialPort("COM7", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
             serialPort1.Open();
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick; ;
+            timerAffichage.Start();
         }
+
+        private void TimerAffichage_Tick(object? sender, EventArgs e)
+        {
+            textboxReception.Text = receivedText;
+            //textboxEmission.Text = "";
+        }
+
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+
+            this.receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+        }
+
 
         private void buttonSend_Click(object sender, RoutedEventArgs e)
         {
@@ -50,15 +70,14 @@ namespace RobotInterface
 
         private void SendMessage()
         {
-            textboxReception.Text += "Reçu :" + textboxEmission.Text + " \n";
+            textboxReception.Text += "Reçu :" + textboxEmission.Text + "\n";
+            serialPort1.WriteLine(textboxEmission.Text);
             textboxEmission.Text = "";
         }
 
         private void buttonClear_Click(object sender, RoutedEventArgs e)
         {
-            textboxReception.Clear();
+            receivedText = "";
         }
-
-
     }
 }
